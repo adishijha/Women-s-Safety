17fede76d90ea2e6cb2e797291a5722114d48d1b
adishijha <40135475+adishijha@users.noreply.github.com>
2021-02-12 17:34:07 +0530

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = ['127.0.0.1','https://womensafetyapplication.herokuapp.com/']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
